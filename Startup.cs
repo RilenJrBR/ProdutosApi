cea8471795c1d077e5a500f906fc8d1356a200ff
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-04-19 21:03:42 -0300

    Implementntando Startup

---
+++ Startup.cs
@@ -0,0 +1,28 @@
+
+namespace ProdutosApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+                    
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
