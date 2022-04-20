98bc3bf019021b7d05bfb6f5fa54b076ce263041
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-04-20 08:36:06 -0300

    Ajustes

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -3,8 +3,8 @@ namespace ProdutosApi.Models
     public class Produto
     {
         public long Id { get; set; }
-        public string? Name { get; set; }
-        public decimal  valor { get; set; }
+        public string? Nome { get; set; }
+        public decimal valor { get; set; }
         public int estoque{ get; set;}
     }
 }
